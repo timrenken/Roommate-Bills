db66bb1264cdb40b98555db6a8c1e57689111127
Tim Renken <tim@timrenken.io>
2022-01-08 17:10:00 -0700

    small fixes

--- reports.py
+++ reports.py
@@ -25,32 +25,32 @@ class PdfReport:
 
         # Insert title
         pdf.set_font(family="Arial", size=24, style="B")
-        pdf.cell(w=0, h=80, txt="Roommates Bill", border=1, align="C", ln=1)
+        pdf.cell(w=0, h=80, txt="Roommates Bill", border=0, align="C", ln=1)
 
         # Insert Period label and value
         pdf.set_font(family="Arial", size=14, style="B")
-        pdf.cell(w=165, h=30, txt="Period:", border=1)
-        pdf.cell(w=150, h=30, txt=bill.period, border=1, ln=1)
+        pdf.cell(w=165, h=30, txt="Period:", border=0)
+        pdf.cell(w=150, h=30, txt=bill.period, border=0, ln=1)
 
         # Insert service label and value
-        pdf.cell(w=200, h=30, txt=f"for: {bill.service}", border=1, ln=1)
+        pdf.cell(w=200, h=30, txt=f"for: {bill.service}", border=0, ln=1)
 
         # Insert name and due amount of the first roommate
         pdf.set_font(family="Arial", size=12)
-        pdf.cell(w=50, h=20, txt=roommate1.name, border=1)
-        pdf.cell(w=150, h=20, txt=f"${roommate1_pay}", border=1, ln=1)
+        pdf.cell(w=50, h=20, txt=roommate1.name, border=0)
+        pdf.cell(w=150, h=20, txt=f"${roommate1_pay}", border=0, ln=1)
 
         # Insert name and due amount of the second roommate
-        pdf.cell(w=50, h=20, txt=roommate2.name, border=1)
-        pdf.cell(w=150, h=20, txt=f"${roommate2_pay}", border=1, ln=1)
+        pdf.cell(w=50, h=20, txt=roommate2.name, border=0)
+        pdf.cell(w=150, h=20, txt=f"${roommate2_pay}", border=0, ln=1)
 
+        # If the reports directory doesn't already exist, create it.
         if os.path.exists("reports"):
             pass
         else:
             os.mkdir("reports")
         
+        # Change directory to reports, and open PDF
         os.chdir("reports")
-
         pdf.output(self.filename)
-
         webbrowser.open(self.filename)
